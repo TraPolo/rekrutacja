28a8654e11b9711134ccde9a5408a7ec011a0989
TraPolo <luk.1997@o2.pl>
2020-03-17 18:06:41 +0100

    initial commit

---
+++ task1.py
@@ -0,0 +1,21 @@
+class Car:
+    def __init__(self, pax_count, car_mass, gear_count):
+        self.pax_count = pax_count
+        self.car_mass = car_mass
+        self.gear_count = gear_count
+
+        if self.pax_count < 1 or self.pax_count > 5 or self.car_mass > 2000:
+            raise IllegalCarError('car doesn\'t meet requirements')
+
+    def total_mass(self):
+        return self.car_mass + self.pax_count * 70
+
+
+
+class IllegalCarError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+car1 = Car(4, 1999, 8)
+car1.pax_count = 8
---
+++ task3.py
@@ -0,0 +1,26 @@
+number_list = [i for i in range(372 ** 2, 809 ** 2+1)]
+
+check = []
+n = 0
+rep_num1 = []
+rep_num2 = []
+finall = []
+for j in number_list:
+    check.append(list(map(int, str(j))))
+
+for i in range(len(check)):
+    rep_num1 = []
+    n = 0
+    for j in range(len(check[i]) - 1):
+        if check[i][j] <= check[i][j + 1]:
+            if check[i][j] == check[i][j + 1]:
+                if check[i][j + 1] not in rep_num1:
+                    rep_num1.append(check[i][j + 1])
+                    n += 1
+                    if n >= 2:
+                        finall.append(number_list[i])
+        else:
+            break
+
+
+print(len(finall))
